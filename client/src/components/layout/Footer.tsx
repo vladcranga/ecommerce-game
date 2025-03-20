19f4a13f136d180fa5e01e559b6cf08cb1e77b3c
vladcranga <vladcranga@yahoo.com>
2025-03-20 15:38:08 +0200

    Improve text contrast and content readability

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -2,9 +2,11 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-4 mt-auto bg-gray-800 text-gray-300">
+    <footer className="w-full py-4 mt-auto bg-gray-800 text-gray-400">
       <div className="container mx-auto text-center">
-        <p className="text-sm">Made by Vlad Mihai Cranga using the MERN stack. © {currentYear}</p>
+        <p className="text-base">
+          Made by Vlad Mihai Cranga using the MERN stack. © {currentYear}
+        </p>
       </div>
     </footer>
   );
